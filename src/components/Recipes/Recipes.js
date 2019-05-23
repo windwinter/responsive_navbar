26ca53e5f1dbb795e9b65a21786be271e29f5f3c
windwinter <windwinter94@gmail.com>
2019-05-23 09:44:39 +0800

    hello yongfan

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -7,9 +7,9 @@ const Recipes = props =>{
         <div className="row">
         {props.recipes.map( recipes => {
           return ( 
-          <div className="col-md-4 col-sm-6"key={recipes.recipes_id}>
+          <div className="col-lg-3 col-md-4 col-sm-6 col-6"key={recipes.recipes_id}>
             <div className="recipe__box">
-                 <img className="recipe_img" style={{width:"100%",height:"auto"}}src={recipes.image_url} alt={recipes.title} />
+                 <img className="recipe_img" style={{width:"100%",height:"150px",}}src={recipes.image_url} alt={recipes.title} />
                  <p >{recipes.title}</p>
             </div>
           </div>
